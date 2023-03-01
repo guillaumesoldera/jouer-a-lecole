4149a0033e7c4e62766b91bfc364a172b97a57e5
Guillaume Soldera <guillaume.soldera@gmail.com>
2023-03-01 18:27:29 +0100

    try to handle browsers which don't support webp format

--- src/components/image.js
+++ src/components/image.js
@@ -5,16 +5,37 @@ export const ImageWithFallback = ({ fallback = noImage, src, className, ...props
     const [isLoaded, setIsLoaded] = useState(false)
     const [imgSrc, setImgSrc] = useState(src)
     const onError = () => setImgSrc(fallback)
+    const imgSrcPng = imgSrc ? imgSrc.split(".")[0] + '.png' : undefined
     return (
         <>
-            <img
-                src={imgSrc ? imgSrc : fallback}
-                onError={onError}
-                {...props}
-                onLoad={() => setIsLoaded(true)}
-                className={isLoaded ? className : 'hide'}
-                loading="lazy"
-            />
+            {imgSrc && (
+                <picture>
+                    <source srcSet={imgSrc} type="image/webp" />
+                    <source srcSet={imgSrcPng} type="image/png" /> 
+                    <img
+                        src={imgSrc}
+                        onError={onError}
+                        {...props}
+                        onLoad={() => setIsLoaded(true)}
+                        className={isLoaded ? className : 'hide'}
+                        loading="lazy"
+                    />
+                </picture>
+                )
+            }
+            {
+                imgSrc === undefined && (
+                    <img
+                        src={fallback}
+                        onError={onError}
+                        {...props}
+                        onLoad={() => setIsLoaded(true)}
+                        className={isLoaded ? className : 'hide'}
+                        loading="lazy"
+                    />
+                )
+            }
+              
             <img
                 src={noImage}
                 {...props}
