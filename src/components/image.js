1a06c4aeb7cf2d4daba65ccb2e4eb4e532da1821
Guillaume Soldera <guillaume.soldera@gmail.com>
2023-03-01 21:14:11 +0100

    2nd try to handle browsers which don't support webp

--- src/components/image.js
+++ src/components/image.js
@@ -4,10 +4,11 @@ import noImage from './no-image.svg';
 export const ImageWithFallback = ({ fallback = noImage, src, className, ...props }) => {
     const [isLoaded, setIsLoaded] = useState(false)
     const [imgSrc, setImgSrc] = useState(src)
+    const imgSrcPng = imgSrc ? imgSrc.split(".")[0] + '.png' : undefined
     const onError = () => setImgSrc(fallback)
     return (
         <>
-            <img
+          {/*  <img
                 src={imgSrc ? imgSrc : fallback}
                 onError={onError}
                 {...props}
@@ -15,6 +16,35 @@ export const ImageWithFallback = ({ fallback = noImage, src, className, ...props
                 className={isLoaded ? className : 'hide'}
                 loading="lazy"
             />
+    */
+    }
+    {imgSrc && (
+                <picture className={isLoaded ? className : ''}>
+                    <source srcSet={imgSrc} type="image/webp" />
+                    <source srcSet={imgSrcPng} type="image/png" /> 
+                    <img
+                        src={imgSrc}
+                        onError={onError}
+                        {...props}
+                        onLoad={() => setIsLoaded(true)}
+                        className={isLoaded ? '' : 'hide'}
+                        loading="lazy"
+                    />
+                </picture>
+                )
+            }
+            {
+                imgSrc === undefined && (
+                    <img
+                        src={fallback}
+                        onError={onError}
+                        {...props}
+                        onLoad={() => setIsLoaded(true)}
+                        className={isLoaded ? className : 'hide'}
+                        loading="lazy"
+                    />
+                )
+            }
             <img
                 src={noImage}
                 {...props}
